773fadbac2666b1e4cc433b360447a6b8fdb78f7
codeforcent <tuankietdxd123@gmail.com>
2021-06-14 01:18:28 +0700

    n

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 
 // // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/DrawEX'));
+app.use(express.static(__dirname + '/dist/DrawEX/browser'));
 
 app.get('/*', (req, res) =>
     res.sendFile(path.join(__dirname + '/dist/DrawEX/browser/index.html'))
