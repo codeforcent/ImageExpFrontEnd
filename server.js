f52c16475a56e0430187f0f33e1769069b94222a
codeforcent <tuankietdxd123@gmail.com>
2021-06-13 22:32:18 +0700

    spr

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+
 
 // // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/DrawEX'));
-
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: __dirname + '/dist/DrawEX/' }),
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 );
 let port = process.env.PORT || 8080;
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
